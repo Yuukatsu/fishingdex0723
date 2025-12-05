f1c10f1171671f0c8b81ccabc1e4cfbd9e6f3039
Yuukatsu <yuukatsu07023@gmail.com>
2025-12-06 01:53:39 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './', // 重要：確保相對路徑正確
+  build: {
+    chunkSizeWarningLimit: 1500, // 提高警告門檻至 1500kb
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // 將 node_modules 中的程式碼獨立打包
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
